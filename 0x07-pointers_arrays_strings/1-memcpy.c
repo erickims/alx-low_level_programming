45586dcc76532a9552864178d8b88570c8be128a
erickims <ericqohkims@gmail.com>
2023-07-04 07:28:19 -0700

    1-memcpy.c task

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,12 +9,12 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int i = 0;
+int i = 0;
 
-	while (i < n)
-        {
-        dest[i] = src[i];
-        i++;
-	}
-	return (dest);
+while (i < n)
+{
+dest[i] = src[i];
+i++;
+}
+return (dest);
 }
